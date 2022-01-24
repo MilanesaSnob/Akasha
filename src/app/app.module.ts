66493ed8452c79d42918a4a76bd64b51e2b2fbc8
Martin Mele <mmmele@gmail.com>
2022-01-24 12:43:41 -0300

    se crea router module y estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HeaderModule } from './shared/components/header/header.module';
+import { ListModule } from './pages/employees/list/list.module';
+import { ListComponent } from './pages/employees/list/list.component';
+import { NewModule } from './pages/employees/new/new.module';
+import { NewComponent } from './pages/employees/new/new.component';
+import { DetailsModule } from './pages/employees/details/details.module';
+import { DetailsComponent } from './pages/employees/details/details.component';
+import { EditModule } from './pages/employees/edit/edit.module';
+import { EditComponent } from './pages/employees/edit/edit.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    NewComponent,
+    DetailsComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HeaderModule,
+    ListModule,
+    NewModule,
+    DetailsModule,
+    EditModule,
+    AppRoutingModule
   
   ],
   providers: [],
